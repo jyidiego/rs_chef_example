31123d469faf72781bded569ae8c745af2dc191f
John Yi <jyidiego@gmail.com>
2012-06-11 19:02:16 +0000

    Tutorial #4 attributes

--- cookbooks/jyi_rs_example/metadata.rb
+++ cookbooks/jyi_rs_example/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures jyi_rs_example"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rs_utils"
+
 recipe "jyi_rs_example::default","Sample Cookbook Deployment, prints 'Hello World!' to the logs"
