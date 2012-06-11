4d0b478b61105bd3141538d378409a245f4d5e5f
John Yi <jyidiego@gmail.com>
2012-06-11 18:09:15 +0000

    Initial commit of jyi_rs_example cookbook

---
+++ cookbooks/jyi_rs_example/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "WebMD"
+maintainer_email "jyi@webmd.net"
+license          "All rights reserved"
+description      "Installs/Configures jyi_rs_example"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "jyi_rs_example::default","Sample Cookbook Deployment, prints 'Hello World!' to the logs"
