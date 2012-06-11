9cad890d8741f140d32100a8dc85d21ee3026e14
John Yi <jyidiego@gmail.com>
2012-06-11 19:20:34 +0000

    Prompt User for First Name

--- cookbooks/jyi_rs_example/attributes/jyi_rs_example.rb
+++ cookbooks/jyi_rs_example/attributes/jyi_rs_example.rb
@@ -1,4 +1,4 @@
-set[:about_me][:first_name] = "John"
+# set[:about_me][:first_name] = "John"
 default[:about_me][:favorite][:food] = "Burrito"
 default[:about_me][:favorite][:drink] = "Macallan"
 default[:about_me][:favorite][:hobby] = "TV"
--- cookbooks/jyi_rs_example/metadata.rb
+++ cookbooks/jyi_rs_example/metadata.rb
@@ -8,3 +8,9 @@ version          "0.0.1"
 depends "rs_utils"
 
 recipe "jyi_rs_example::default","Sample Cookbook Deployment, prints 'Hello World!' to the logs"
+
+attribute "about_me/first_name",
+  :required => "required",
+  :display_name => "First Name",
+  :description => "Sufficiently described",
+  :recipes => ["jyi_rs_example::default"]
